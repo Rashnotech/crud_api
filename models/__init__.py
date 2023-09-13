8dc50b12755dea7f2980436970f89e4804118d12
Rashnotech <rashnotech@gmail.com>
2023-09-13 12:14:10 +0100

    Implemented routing for crud api

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,11 @@
 """ a module that instantiate the package imports """
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.app_context().push()
 from models import routes
--- models/routes.py
+++ models/routes.py
@@ -1,11 +1,55 @@
 """ a module that handle the routing of the api """
-
-
-from models import app
+from models import app, request, jsonify, db
 from models.User import User
 
 
-@app.route('/api')
-def hello_world():
-    """ a function that handles api calls"""
-    return ('<h1>Hello world</h1>')
+@app.route('/api', methods=['POST', 'GET'])
+@app.route('/api/create', methods=['POST'])
+def create():
+    """ a function that handles create function"""
+    if request.method == 'GET':
+        return jsonify({})
+    
+    if request.method == 'POST':
+        name = request.form['name']
+        user = User(name)
+        db.session.add(user)
+        db.session.commit()
+
+        return (jsonify({}))
+
+@app.route('/api/<int:id>')
+def read(id):
+    """ a function that read user details from db """
+    user = User.query.filter_by(user_id=id).first()
+    if user:
+        return jsonify({})
+    return jsonify({})
+
+@app.route('/api/<int:id>/update', methods=['GET', 'POST'])
+def update(id):
+    """ a function that update user details in db """
+    user = User.query.filter_by(user_id=id)
+    if request.method == 'POST':
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            name = request.form['name']
+            user = User(name=name)
+
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({})
+        return jsonify({})
+    return jsonify({})
+
+@app.route('/api/<int:id>/delete', methods=['GET', 'POST'])
+def delete(id):
+    """ a function that delete details from db"""
+    user = User.query.filter_by(user_id=id)
+    if request.method == 'POST':
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return jsonify({})
+        return jsonify({})
